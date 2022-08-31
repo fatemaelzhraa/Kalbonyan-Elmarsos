c4a99a0209bbe6b6b874e620d137dadc7b3b11d9
tlafrik <109369193+fatemaelzhraa@users.noreply.github.com>
2022-08-31 11:46:50 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/02-Challenges/challenge2.js
@@ -0,0 +1,27 @@
+// const massMark = 95;
+// const massJohn = 85;
+// const heightMark = 1.88;
+// const heightJohn = 1.76;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const heightMarkBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn);
+// const massMark = 78;
+// const massJohn = 92;
+// const heightMark = 1.69;
+// const heightJohn = 1.95;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const heightMarkBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, heightMarkBMI);
+
+// if (BMIMark > BMIJohn) {
+//   console.log("Mark's BMI is higher than John's!");
+// } else {
+//   console.log(" John's BMI is higher than Mark's !");
+// }
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
