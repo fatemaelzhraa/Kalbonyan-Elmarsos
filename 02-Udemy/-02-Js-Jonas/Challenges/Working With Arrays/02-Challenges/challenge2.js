f4bcc91956b26980250ed56f56b16af27dd179c8
tlafrik <109369193+fatemaelzhraa@users.noreply.github.com>
2022-08-31 12:03:03 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/Working With Arrays/02-Challenges/challenge2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+  console.log(adults);
+  return average;
+};
+const data1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const data2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(data1, data2);
