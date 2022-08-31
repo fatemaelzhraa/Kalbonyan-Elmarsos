75f2bd680b8816bbf4e17c8dbc4b6bad57081604
tlafrik <109369193+fatemaelzhraa@users.noreply.github.com>
2022-08-31 11:47:16 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 1/03-Challenges/challenge3.js
@@ -0,0 +1,63 @@
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scorekoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scorekoalas);
+
+// if (scoreDolphins > scorekoalas) {
+//   console.log("Dolphins win the troph 🏆");
+// } else if (scorekoalas > scoreDolphins) {
+//   console.log("Koalas win the troph 🏆");
+// } else if (scoreDolphins === scorekoalas) {
+//   console.log("Both win the trophy!");
+// }
+
+// const scoreDolphins = (97 + 112 + 110) / 3;
+// const scorekoalas = (109 + 95 + 123) / 3;
+// console.log(scoreDolphins, scorekoalas);
+
+// if (scoreDolphins > scorekoalas && scoreDolphins >= 100) {
+//   console.log("Dolphins win the troph 🏆");
+// } else if (scorekoalas > scoreDolphins && scorekoalas >= 100) {
+//   console.log("Koalas win the troph 🏆");
+// } else if (
+//   scoreDolphins === scorekoalas &&
+//   scoreDolphins >= 100 &&
+//   scorekoalas >= 100
+// ) {
+//   console.log("Both win the trophy!");
+// } else {
+//   console.log("No one wins the trophy 😭");
+// }
+// const scoreDolphins = (97 + 112 + 102) / 3;
+// const scorekoalas = (109 + 95 + 50) / 3;
+// console.log(scoreDolphins, scorekoalas);
+
+// if (scoreDolphins > scorekoalas && scoreDolphins >= 100) {
+//   console.log("Dolphins win the troph 🏆");
+// } else if (scorekoalas > scoreDolphins && scorekoalas >= 100) {
+//   console.log("Koalas win the troph 🏆");
+// } else if (
+//   scoreDolphins === scorekoalas &&
+//   scoreDolphins >= 100 &&
+//   scorekoalas >= 100
+// ) {
+//   console.log("Both win the trophy!");
+// } else {
+//   console.log("No one wins the trophy 😭");
+// }
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scorekoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scorekoalas);
+
+if (scoreDolphins > scorekoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the troph 🏆");
+} else if (scorekoalas > scoreDolphins && scorekoalas >= 100) {
+  console.log("Koalas win the troph 🏆");
+} else if (
+  scoreDolphins === scorekoalas &&
+  scoreDolphins >= 100 &&
+  scorekoalas >= 100
+) {
+  console.log("Both win the trophy!");
+} else {
+  console.log("No one wins the trophy 😭");
+}
