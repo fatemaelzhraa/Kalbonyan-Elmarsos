c37fd8302b260ffe470c3fa083890c63082f5741
tlafrik <109369193+fatemaelzhraa@users.noreply.github.com>
2022-08-31 12:28:40 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Projects/Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8b904e06-5ca3-4d52-8641-e29bed479a06';
+export const MODAL_CLOSE_SEC = 2.5;
