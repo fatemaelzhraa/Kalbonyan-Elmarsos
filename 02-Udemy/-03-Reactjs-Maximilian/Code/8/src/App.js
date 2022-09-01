1ed49102fa28d118e084e7077ff7fbedde595233
tlafrik <109369193+fatemaelzhraa@users.noreply.github.com>
2022-09-01 10:26:32 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Code/8/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import AddUser from "./components/UI/Users/AddUser";
+import UsersList from "./components/UI/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.round().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
